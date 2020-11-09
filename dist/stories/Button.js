361e919ae80ffeb5f5744a2f27f418a914ad3bcb
xi1894-amreshmishra <amresh.mishra@xebia.com>
2020-11-09 11:51:14 +0530

    state add

--- dist/stories/Button.js
+++ dist/stories/Button.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
+
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
@@ -7,9 +9,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Button = void 0;
 
+var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/esm/slicedToArray"));
+
 var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/esm/objectWithoutProperties"));
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 require("./button.css");
 
@@ -22,14 +26,26 @@ var Button = function Button(_ref) {
       size = _ref.size,
       label = _ref.label,
       props = (0, _objectWithoutProperties2.default)(_ref, ["primary", "backgroundColor", "size", "label"]);
+
+  var _useState = (0, _react.useState)(backgroundColor),
+      _useState2 = (0, _slicedToArray2.default)(_useState, 2),
+      color = _useState2[0],
+      setColor = _useState2[1];
+
+  console.log(color);
   var mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return /*#__PURE__*/_react.default.createElement("button", Object.assign({
+  return /*#__PURE__*/_react.default.createElement("button", {
     type: "button",
     className: ['storybook-button', "storybook-button--".concat(size), mode].join(' '),
-    style: backgroundColor && {
-      backgroundColor: backgroundColor
-    }
-  }, props), label);
+    style: color && {
+      backgroundColor: color
+    },
+    onClick: function onClick() {
+      console.log("clicked");
+      setColor("red");
+    } // {...props}
+
+  }, label);
 };
 
 exports.Button = Button;
--- src/stories/Button.js
+++ src/stories/Button.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
 
@@ -6,13 +6,19 @@ import './button.css';
  * Primary UI component for user interaction
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+  const [color, setColor] = useState(backgroundColor)
+  console.log(color)
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
+      style={color && { backgroundColor:color }}
+      onClick={()=>{
+        console.log("clicked")
+        setColor("red")
+      }}
+      // {...props}
     >
       {label}
     </button>
